1e3f184c7bfa3090cb0ceb09f2d23abb75d7ce0f
shenghangluo <44126231+shenghangluo@users.noreply.github.com>
2023-03-04 20:08:55 +0800

    Delete FNN_AM_PB_NLC directory

--- FNN_AM_PB_NLC/Pytorch_code/config.py
+++
@@ -1,7 +0,0 @@
-def create_params():
-    n_epochs = 500
-    tr_batch_size = 100
-    optimizer_params = {'lr': 1e-2}
-    return {'n_epochs': n_epochs,
-            'tr_batch_size': tr_batch_size,
-            'optimizer_params': optimizer_params}
