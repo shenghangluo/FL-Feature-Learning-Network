684f956add86850119963683e58f3dfb74e99346
shenghangluo <44126231+shenghangluo@users.noreply.github.com>
2023-03-04 20:09:32 +0800

    Delete NN_PB_NLC/MATLAB_CODE directory

--- NN_PB_NLC/MATLAB_CODE/nonlinear_coeff_zero_zero.m
+++
@@ -1,4 +0,0 @@
-function q=nonlinear_coeff_zero_zero(tto,beta2,L)
-fun = @(z)(1./sqrt((tto^4/(3*beta2^2))+z.^2));
-q = integral(fun,0,L);
-end
--- NN_PB_NLC/MATLAB_CODE/xQAMsignal.m
+++
@@ -1,8 +0,0 @@
-function f = xQAMsignal(Sequence_baud, Nsamples_baud)
-
-signal = zeros(1, Nsamples_baud*length(Sequence_baud)); 
-%Sequence_baud = Sequence_baud*exp(1i*Offset_baud);
-for iter = 1:length(Sequence_baud)
-    signal([((iter-1)*Nsamples_baud+1):(iter*Nsamples_baud)]) = Sequence_baud(iter);
-end; 
-f = signal;
